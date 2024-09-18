484f70ffacabd772875656bceb2057aeb5d92f4f
anyasuerte1 <anyasuerte@gmail.com>
2024-09-18 15:18:42 +0400

    implemented reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const isNumberToArr = n.toString().split('');
+  const result = [];
+  for(let i = isNumberToArr.length - 1; i >= 0; i = i - 1) {
+    result.push(isNumberToArr[i]);
+  }
+  return nunberReverse = parseInt(result.join(''));
 }
+
